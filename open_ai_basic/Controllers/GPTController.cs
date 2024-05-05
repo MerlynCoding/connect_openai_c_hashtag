6c1649899e0e2607d0396b72b74aaa308a39bab2
MerlynCoding <144709971+MerlynCoding@users.noreply.github.com>
2024-05-05 16:28:02 +0800

    Add files via upload

---
+++ open_ai_basic/Controllers/GPTController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Http.HttpResults;
+using Microsoft.AspNetCore.Mvc;
+using OpenAI_API.Completions;
+
+namespace open_ai_basic.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class GPTController : Controller
+    {
+        [HttpGet("OpenAI")]
+        public async Task<IActionResult> OpenAI()
+        {
+            string Repon = "";
+            string api = "";
+
+            var client = new OpenAI_API.OpenAIAPI(api);
+            CompletionRequest completionRequest = new CompletionRequest();
+            completionRequest.Prompt = "";
+            completionRequest.Model = OpenAI_API.Models.Model.DavinciText;
+            completionRequest.MaxTokens = 1024;
+
+            var completionOpenAi= await client.Completions.CreateCompletionAsync(completionRequest);
+            foreach (var completion in completionOpenAi.Completions)
+            {
+                Repon += completion.Text;
+            }
+
+
+            return Ok(Repon);
+        }
+    }
+}
